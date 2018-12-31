1107a6400a71ac376348aca90aeb5952554acd54
DiaZzzFF <dvsh.dreamer@gmail.com>
2018-12-31 04:21:39 +0300

    Добавляет задание_4

--- js/pin.js
+++ js/pin.js
@@ -8,9 +8,11 @@
     var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
     var pinElement = pinTemplate.cloneNode(true);
 
-    pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
-    pinElement.querySelector('img').src = pin.author.avatar;
-    pinElement.querySelector('img').alt = pin.offer.title;
+    if (pin.offer) {
+      pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+      pinElement.querySelector('img').src = pin.author.avatar;
+      pinElement.querySelector('img').alt = pin.offer.title;
+    }
 
     // Обработчик для открытия (объявления)
     pinElement.addEventListener('click', function () {
